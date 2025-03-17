4743710dda03c1ae250878b91badb23465fbc5ef
TeamFamilyTasker <projecthadasteam6@gmail.com>
2025-03-17 19:30:59 +0200

    Multply function

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 def add_numbers(a, b):
     return a + b
 
+def multiply_numbers(a, b):
+    return a * b
+
 if __name__ == "__main__":
 print("This is a simple addition function")
     print(f"3 + 5 = {add_numbers(3, 5)}")
